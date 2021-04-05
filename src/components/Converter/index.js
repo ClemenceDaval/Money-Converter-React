b6a02c17e52e8d450de23edf3a64deaf1cdd9886
Lagaalett <daval.clemence@gmail.com>
2021-04-05 17:39:34 +0200

    conversion of Converter component from functionnal component to class component. Addition of state

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prefer-stateless-function */
 // == Import npm
 import React from 'react';
 
@@ -10,14 +11,22 @@ import Amount from '../Amount';
 
 import './converter.scss';
 
-// == Composant
-const Converter = () => (
-  <div className="converter">
-    <Header baseAmount={1} />
-    <Currencies currencies={currenciesData} />
-    <Amount amount={1.10} currency="United States Dollar" />
-  </div>
-);
+class Converter extends React.Component {
+  state = {
+    baseAmount: 1,
+    currency: 'United States Dollar',
+  };
+
+  render() {
+    return (
+      <div className="converter">
+        <Header baseAmount={1} />
+        <Currencies currencies={currenciesData} />
+        <Amount amount={1.10} currency="United States Dollar" />
+      </div>
+    );
+  }
+}
 
 // == Export
 export default Converter;
